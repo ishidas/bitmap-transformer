a4ec7ca0554dba776b562b560ef110e7d22e201f
Sawako Ishida <sawako.yoriko@gmail.com>
2016-03-06 01:01:56 -0800

    bitmap object created with readFile data

--- bitmap-obj.js
+++ bitmap-obj.js
@@ -1,18 +1,18 @@
 'use strict';
-var imgBuffer = require( __dirname + '/img-buffer.js');
-var bitmap = imgBuffer.bitmap;
 var events = require('events');
 var emitter = new events.EventEmitter();
 
+console.log('Am I here?');
 
 emitter.on('createBitmapObj',exports.createBitmapObj = function(bitmap){
+
   console.log('here here');
   console.dir(bitmap);
-  bitmap.headField = bitmap.buf.toString('ascii', 0,2);
-  // console.log('I am the: ' + bitmap);
-  bitmap.size = bitmap.buf.readUInt32LE(2);
-  bitmap.startPixelArray = bitmap.buf.readUInt32LE(10);
-  bitmap.colorPalette = bitmap.buf.readUInt32LE(46);
+  bitmap.headField = bitmap.toString('ascii', 0,2);
+  console.log('I am the: ' + bitmap);
+  bitmap.size = bitmap.readUInt32LE(2);
+  bitmap.startPixelArray = bitmap.readUInt32LE(10);
+  bitmap.colorPalette = bitmap.readUInt32LE(46);
   console.dir('headField :' + bitmap.headField);
   console.dir('size :' + bitmap.size);
   console.dir('Pix Array start :' + bitmap.startPixelArray);
--- img-buffer.js
+++ img-buffer.js
@@ -1,24 +1,21 @@
 'use strict';
 var fs = require('fs');
+var bitmapObjFile = require( __dirname + '/bitmap-obj.js');
 var events = require('events');
 var emitter = new events.EventEmitter();
 
-var bitmap = {};
+
 
 //reading and writing files using fs readFile & writeFile method
-fs.readFile( __dirname + '/img/palette-bitmap.bmp', function(err, data){
-  console.log(data);
+fs.readFile( __dirname + '/img/palette-bitmap.bmp',function(err, data){
+  var bitmap = {};
 
   emitter.on('saveBuffer', function(data){
-    bitmap.buf = data;
-    // console.log(`I am here :  ${bitmapBuf}`);
-    fs.writeFile( __dirname + '/img/color.bmp', bitmap.buf);
+    bitmap = data;
+    fs.writeFile( __dirname + '/img/color.bmp',bitmap);
+    bitmapObjFile.createBitmapObj(bitmap);
   });
+
   emitter.emit('saveBuffer', data);
-  // emitter.emit('createBitmapObj');
-  // console.dir(bitmap);
+  
 });
-
-
-
-module.bitmap = bitmap;
